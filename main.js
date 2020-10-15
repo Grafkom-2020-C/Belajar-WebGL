02866b4ff4a2d611b4236f7271ca4677756cb238
Hadziq Fabroyir <hadziq@gmail.com>
2020-10-15 09:02:03 +0700

    Berhasil menggambar persegi dengan gl.TRIANGLE_STRIP

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ function main() {
    * D = (-0.5, -0.5)
    */
   var vertices = [
-    -0.5, 0.5,      // Titik A
     0.5, 0.5,       // Titik B
+    -0.5, 0.5,      // Titik A
     0.5, -0.5,      // Titik C
-    -0.5, -0.5       // Titik D
+    -0.5, -0.5      // Titik D
   ];
 
   var vertexBuffer = gl.createBuffer();
@@ -66,8 +66,9 @@ function main() {
 
   gl.clearColor(0.0, 0.22, 0.5, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.viewport(100, 0, canvas.height, canvas.height);
 
-  var primitive = gl.TRIANGLE_FAN;
+  var primitive = gl.TRIANGLE_STRIP;
   var offset = 0;
   var nVertex = 4;
   gl.drawArrays(primitive, offset, nVertex);
