595cb9909938c00d1e8868918d00b76e34e1374d
Hadziq Fabroyir <hadziq@gmail.com>
2021-01-06 06:02:45 +0700

    Memutar kubus beserta sumbu putarnya dengan mouse

--- main.js
+++ main.js
@@ -210,9 +210,53 @@ function main() {
   gl.uniform3fv(uLightPosition, [2.0, 3.0, 2.0]);
   var uNormal = gl.getUniformLocation(shaderProgram, 'u_Normal');
 
+  // Memutar kubus secara euclidean menggunakan mouse
+  var rotation = glMatrix.mat4.create();
+  var dragging, lastx, lasty;
+  function onMouseDown(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var rect = event.target.getBoundingClientRect();
+    // Saat mouse diklik-kiri di area aktif browser,
+    //  maka flag dragging akan diaktifkan
+    if (
+      rect.left <= x &&
+      rect.right > x &&
+      rect.top <= y &&
+      rect.bottom > y
+    ) {
+      dragging = true;
+      lastx = x;
+      lasty = y;
+    }
+  }
+  function onMouseUp(event) {
+    // Ketika klik-kiri mouse dilipas
+    dragging = false;
+  }
+  function onMouseMove(event) {
+    if (dragging) {
+      var x = event.clientX;
+      var y = event.clientY;
+      // Asumsinya geser 1 piksel = putar 1/2 derajat
+      var dx = (x - lastx) / 2;
+      var dy = (y - lasty) / 2;
+      var radx = glMatrix.glMatrix.toRadian(dy); // Rotasi terhadap sumbu x sebesar dy
+      var rady = glMatrix.glMatrix.toRadian(dx); // Rotasi terhadap sumbu y sebesar dx
+      // Menggunakan dx dan dy untuk memutar kubus
+      glMatrix.mat4.rotate(rotation, rotation, radx, [1, 0, 0, 0]); // rotasi terhadap sumbu x
+      glMatrix.mat4.rotate(rotation, rotation, rady, [0, 1, 0, 0]); // rotasi terhadap sumbu y
+    }
+    lastx = x;
+    lasty = y;
+  }
+  document.addEventListener('mousedown', onMouseDown);
+  document.addEventListener('mouseup', onMouseUp);
+  document.addEventListener('mousemove', onMouseMove);
+
   function render() {
-    glMatrix.mat4.rotate(model, model, glMatrix.glMatrix.toRadian(0.5), [0.0, 0.0, 1.0]);
-    glMatrix.mat4.rotate(model, model, glMatrix.glMatrix.toRadian(1), [0.0, 1.0, 0.0]);
+    model = glMatrix.mat4.create(); // Matriks model kita reset ulang setiap kali render
+    glMatrix.mat4.multiply(model, model, rotation);
     gl.uniformMatrix4fv(uModel, false, model);
     var normal = glMatrix.mat3.create();
     glMatrix.mat3.normalFromMat4(normal, model);
